8f5d50410642a87c1177104fb5393623a9c57abb
tuheen66 <hassan.monirul@gmail.com>
2024-06-04 04:31:06 +0600

    added banner slider - swiper-slider to site

---
+++ src/Components/BannerSlider.jsx
@@ -0,0 +1,51 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+import slider1 from "../assets/images/slide-1.jpg";
+import slider2 from "../assets/images/slide-2.jpg";
+import slider3 from "../assets/images/slide-3.jpg";
+import slider4 from "../assets/images/slide-4.jpg";
+import slider5 from "../assets/images/slide-5.jpg";
+
+const BannerSlider = () => {
+  return (
+    <div>
+      <Swiper
+        className="rounded-xl"
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        <SwiperSlide>
+          <img src={slider1} className="w-full h-screen " />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slider2} className="w-full h-screen " />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slider3} className="w-full h-screen " />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slider4} className="w-full h-screen " />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slider5} className="w-full h-screen" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default BannerSlider;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import { useLoaderData } from "react-router-dom";
 import PropertyCard from "../../Components/PropertyCard";
+import BannerSlider from "../../Components/BannerSlider";
 
 const Home = () => {
   const properties = useLoaderData();
   return (
     <div className="w-[90%] mx-auto">
+
+        <BannerSlider></BannerSlider>
+
+
       <h2 className="my-8 font-bold text-3xl text-center text-gray-700">Estates</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-20">
